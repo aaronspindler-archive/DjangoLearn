f60bc2b230578b6f76619f3b7c450acb343ac0ae
Aaron Spindler <nova1104@live.com>
2019-06-05 15:44:26 -0400

    Finished blog in portfolio

--- portfolio-project/blog/views.py
+++ portfolio-project/blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Blog
 
 def allblogs(request):
-    return render(request, 'blog/allblogs.html')
+    blogs = Blog.objects
+    return render(request, 'blog/allblogs.html', {'blogs':blogs})
