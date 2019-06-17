ff8d30db76500d2d9b99499ded6158b95202d325
Aaron Spindler <nova1104@live.com>
2019-06-17 15:00:51 -0400

    Created signup page for product hunt

--- producthunt-project/accounts/views.py
+++ producthunt-project/accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
-# Create your views here.
+def signup(request):
+    #The user wants to sign up
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username = request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error':'Username has already been taken!'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                auth.login(request,user)
+                return redirect('home')
+        else:
+            return render(request, 'accounts/signup.html', {'error':'Passwords must match!'})
+
+                #User wants to enter info
+    else:
+        return render(request, 'accounts/signup.html')
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    # TODO Need to route to homepage
+    # and don't forget to logout
+    return render(request, 'accounts/signup.html')
